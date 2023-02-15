d75524cb11e7805bc5faccbd06c3fe40ac2929ef
Rinny N <artpr.zxc@gmail.com>
2023-02-15 23:05:44 +0500

    Revert "[add]: New slash command by reload cogs with the check that an author command is me"
    This reverts commit 4310be2dfb7d239da782257a13ce15af82838ba1.

--- bot/cogs/other.py
+++ bot/cogs/other.py
@@ -1,7 +1,4 @@
-from nextcord.ext.commands import Bot, Cog, ExtensionNotFound, ExtensionNotLoaded, ExtensionFailed, NoEntryPointError
-from nextcord import Interaction
-import os
-from bot.misc import Config
+from nextcord.ext.commands import Bot, Cog, Context
 
 
 # todo: OtherCogs
@@ -13,51 +10,6 @@ class __MainOtherCog(Cog):
     @Cog.listener()
     async def on_ready(self) -> None:
         print('I am ready!!!')
-
-    @Bot.slash_command(Bot())
-    async def reload(self, interaction: Interaction, *, cogs: str = ''):
-        """Using for reload the bot cogs. Cogs are separated by space."""
-        # Check on author is me
-        if interaction.user.id != Config.ID_ME:
-            await interaction.response.send_message('You cannot use this command', ephemeral=True)
-            return
-
-        # Finding cogs
-        if not cogs:
-            for (dir_path, dir_names, file_names) in os.walk('./bot/cogs/'):
-
-                # Add a cog name to the list
-                for name in file_names:
-                    if os.path.splitext(name)[1] == '.py':
-                        path = dir_path[6:].replace('/', '.')
-                        cogs += ' ' if cogs else '' + f'{path}.{os.path.splitext(name)[0]}'
-
-        # Logging and Reloading Extension
-        log, success = [], 0
-        for cog in cogs.split(' '):
-            try:
-                self.bot.reload_extension(cog)
-            except ExtensionNotLoaded as exc:
-                log.append(exc.args[0])
-            except ExtensionNotFound as exc:
-                log.append(exc.args[0])
-            except NoEntryPointError as exc:
-                log.append(exc.args[0])
-            except ExtensionFailed as exc:
-                log.append(exc.args[0])
-
-            else:
-                success += 1
-
-        # Outputs a result by reload
-        await interaction.response.send_message(
-            '**All extensions have been reload successfully**' if not log else
-
-            '\n'.join(['• ' + err.replace("'", '`') for err in log])
-            + f'\n**Successfully reload {success} extensions**',
-
-            ephemeral=True
-        )
 
 
 def register_other_cogs(bot: Bot) -> None:
--- bot/misc/config.py
+++ bot/misc/config.py
@@ -4,4 +4,3 @@ from typing import Final
 
 class Config(ABC):
     CMD_PREFIX: Final = '.'
-    ID_ME: Final = 1053981088449581077
