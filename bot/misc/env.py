0a6e069e33e2393aa662ff9ad35f72b4daa846fb
ScpBill <artpr.zxc@gmail.com>
2023-02-20 21:10:31 +0500

    Change the method to get environment variables

--- bot/misc/env.py
+++ bot/misc/env.py
@@ -1,20 +1,12 @@
-# == PyCharm version ==
+# == Getting the secret information ==
 import os
 from abc import ABC
 from typing import Final
+from dotenv import load_dotenv
+
+
+load_dotenv('./.env')
 
 
 class Env(ABC):
-    TOKEN: Final = os.environ.get('TOKEN', 'define me!')
-
-
-# == PythonAnywhere ==
-# ...
-# from dotenv import load_dotenv
-#
-#
-# load_dotenv('.env')
-#
-#
-# class Env(ABC):
-#     TOKEN: Final = os.getenv('TOKEN')
+    TOKEN: Final = os.environ.get('TOKEN', '0_0')
