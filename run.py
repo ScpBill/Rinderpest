c5e7b70fe01bf5aa216cf04f2f978e921da61e86
ScpBill <artpr.zxc@gmail.com>
2023-02-20 21:40:27 +0500

    Add the inputimeout to method to get environment variables

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
 import os
-
-os.system('git pull origin master')
+from inputimeout import inputimeout, TimeoutOccurred
 
 
 if __name__ == '__main__':
-    from bot import start_bot
+    print('==> Warning <==')
 
+    try:
+        inputimeout(prompt='> ', timeout=10)
+    except TimeoutOccurred:
+        os.system('git pull origin master')
+    else:
+        while True:
+            if (msg := input('> ')) != 'exit':
+                os.system(msg)
+            else:
+                break
+
+    from bot import start_bot
     start_bot()
