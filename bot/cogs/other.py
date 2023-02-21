407802659aef981e1007f76f1693fc17b92a2759
ScpBill <artpr.zxc@gmail.com>
2023-02-21 22:44:26 +0500

    Add: new method to update bot by input the update command in discord

--- bot/cogs/other.py
+++ bot/cogs/other.py
@@ -1,4 +1,6 @@
-from nextcord.ext.commands import Bot, Cog, Context
+from nextcord.ext.commands import Bot, Cog
+from nextcord import Interaction
+from git.repo import Repo
 
 
 # todo: OtherCogs
@@ -10,6 +12,17 @@ class __MainOtherCog(Cog):
     @Cog.listener()
     async def on_ready(self) -> None:
         print('I am ready!!!')
+
+    @Bot.slash_command(Bot(), 'update')
+    async def update(self, interaction: Interaction):
+        reply = await interaction.response.send_message(
+            'Please wait...', ephemeral=True
+        )
+
+        repo = Repo('/')
+        res = repo.remotes.origin.pull()
+
+        await reply.edit(str(res))
 
 
 def register_other_cogs(bot: Bot) -> None:
