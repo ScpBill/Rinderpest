b7a46ab7b81e559906cd9f9ecde9ac4fa65e29c9
ScpBill <artpr.zxc@gmail.com>
2023-02-22 12:54:41 +0500

    Improved: describe in update function

--- bot/cogs/other.py
+++ bot/cogs/other.py
@@ -20,16 +20,19 @@ class __MainOtherCog(Cog):
 
     @Bot.slash_command(Bot(), 'update')
     async def update(self, interaction: Interaction):
+        """Updating data via a remote git repository"""
         # Check on author is me
         if interaction.user.id != Config.ID_ME:
             await interaction.response.send_message('You cannot use this command', ephemeral=True)
             return
 
+        # Waiting message
         reply = await interaction.response.send_message(
             'Please wait...', ephemeral=True
         )
         msg, log = 'The progress of reading data from a remote server', []
 
+        # Generate commands
         await reply.edit(
             msg := msg + '\n• Git reset finish the code `{}`'.format(
                 code_1 := os.system('git reset --hard')))
@@ -42,6 +45,7 @@ class __MainOtherCog(Cog):
             msg := msg + '\n• Git pull origin finish the code `{}`'.format(
                 code_3 := os.system('git pull origin master')))
 
+        # Main result message
         if all(code == 0 for code in (code_1, code_2, code_3)):
             await reply.edit(msg + '\n**The git repository has been successfully updated!**')
         else:
