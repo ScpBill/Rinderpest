741fe480399a427666da0e8866143e68cad27025
ScpBill <artpr.zxc@gmail.com>
2023-02-22 23:32:47 +0500

    Change: files path directory [2]

--- bot/cogs/admin/__init__.py
+++ bot/cogs/admin/__init__.py
@@ -4,7 +4,7 @@ import os
 
 def setup_admin_cogs(bot: Bot) -> None:
     """Setup cogs by loading extensions"""
-    files: list = os.listdir('.\\bot\\cogs\\admin')
+    files: list = os.listdir('.\\bot\\cogs\\admin\\')
     files.remove('__init__.py')
 
     bot.load_extensions([f'bot.cogs.admin.' + os.path.splitext(file)[0] for file in files if file.endswith('.py')])
