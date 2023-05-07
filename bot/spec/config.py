2f57afd90d52298ad3928711925ff231758525b8
ScpBill <artpr.zxc@gmail.com>
2023-05-08 00:19:10 +0500

    Change: config info (remove id_me, add standard_extensions)

--- bot/spec/config.py
+++ bot/spec/config.py
@@ -4,6 +4,5 @@ from typing import Final
 
 class Config(ABC):
     CMD_PREFIX: Final = '.'
-    ID_ME: Final = 1053981088449581077
     ID_GUILD: Final = 1101622403693547630
-    STANDARD_COGS: Final = []
+    STANDARD_EXTENSIONS: Final = ['admin.loader', 'server.general']
