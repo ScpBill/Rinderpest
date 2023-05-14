a275e6ae3b0253b88726038ab243166d12fca9e1
ScpBill <artpr.zxc@gmail.com>
2023-05-14 15:54:23 +0500

    Update: presence.py

--- bot/cogs/owner/presence.py
+++ bot/cogs/owner/presence.py
@@ -1,6 +1,8 @@
-from discord.ext.commands import Bot, Cog
+from discord.ext.commands import Bot, Cog, Context
 from discord.ext.tasks import loop
-from discord import Activity
+from discord.ext import commands
+from discord import Activity, Game
+from pypresence import AioPresence
 
 from time import time
 
@@ -11,18 +13,18 @@ class Delay:
 
     @property
     def days(self):
-        _d = str(self._delay // 86400)
+        _d = str(int(self._delay // 86400))
         return f'{_d} day{"" if _d.endswith("1") else "s"}'
 
     @property
     def hours(self):
-        _h = str(self._delay // 3600 % 24)
-        return f'{_h} day{"" if _h.endswith("1") else "s"}'
+        _h = str(int(self._delay // 3600 % 24))
+        return f'{_h} hour{"" if _h.endswith("1") else "s"}'
 
     @property
     def minutes(self):
-        _m = str(self._delay // 60 % 60)
-        return f'{_m} day{"" if _m.endswith("1") else "s"}'
+        _m = str(int(self._delay // 60 % 60))
+        return f'{_m} minute{"" if _m.endswith("1") else "s"}'
 
 
 class RichPresenceCog(Cog, name='Rich Presence', description='Manage bot\'s and owners rich presences'):
@@ -30,27 +32,62 @@ class RichPresenceCog(Cog, name='Rich Presence', description='Manage bot\'s and 
     def __init__(self, bot: Bot):
         self.bot = bot
         self.start_time = time()
+        self.information = self._generator_statuses()
+        self.presence = AioPresence(client_id=self.bot.application_id, loop=self.bot.loop)
 
-    @property
+    @commands.hybrid_group(name='rp')
+    async def _rich_presence(self, ctx: Context) -> None: pass
+
+    @_rich_presence.command(name='update', description='Change my rich presence')
+    @commands.is_owner()
+    async def _change_presence(
+            self, ctx: Context, state: str = None, details: str = None, start: int = None, end: int = None,
+            large_image: str = None, large_text: str = None, small_image: str = None, small_text: str = None,
+            party_id: str = None, party_size: str = None, join: str = None, spectate: str = None,
+            match: str = None, buttons: str = None, instance: bool = True) -> None:
+
+        await ctx.defer(ephemeral=True)
+        await self.presence.clear()
+
+        try:
+            await self.presence.update(
+                state=state, details=details, start=start, end=end, large_image=large_image, large_text=large_text,
+                small_image=small_image, small_text=small_text, party_id=party_id,
+                party_size=eval(party_size) if party_size is not None else None, join=join,
+                spectate=spectate, match=match, buttons=eval(buttons) if buttons is not None else None, instance=instance
+            )
+        except Exception as exc:
+            await ctx.reply(f'{type(exc).__name__}: {exc}', mention_author=False)
+        else:
+            await ctx.reply('Your status is successful changed!', mention_author=False)
+
+    @_rich_presence.command(name='clear', description='Clear my rich presence')
+    @commands.is_owner()
+    async def _clear_presence(self, ctx: Context) -> None:
+        await ctx.defer(ephemeral=True)
+        await self.presence.clear()
+
+    def _generator_statuses(self):
+        while True:
+            yield 'Passed: {} {} {}'.format(self.delay().days, self.delay().hours, self.delay().minutes)
+            yield '/help | .help | @{} help'.format(self.bot.application.name)
+
     def delay(self) -> Delay:
         return Delay(time() - self.start_time)
 
     @Cog.listener()
     async def on_ready(self) -> None:
-        self.update_status.stop()
-        self.update_status.start()
+        await self.presence.connect()
+        await self.bot.wait_until_ready()
+        print('[+] Rich Presence Cog is started')
+
+        self._update_status.stop()
+        self._update_status.start()
 
     @loop(seconds=10)
-    async def update_status(self) -> None:
-        def info():
-            while True:
-                yield 'Passed: {} {} {}'.format(self.delay.days, self.delay.hours, self.delay.minutes)
-                yield '@{} help | /help | .help'.format(self.bot.application.name)
-
-        information = info()
-
-        await self.bot.change_presence(activity=Activity(
-            name=next(information), state='Bot is '
+    async def _update_status(self) -> None:
+        await self.bot.change_presence(activity=Game(
+            name=next(self.information)
         ))
 
 
--- bot/spec/config.py
+++ bot/spec/config.py
@@ -5,4 +5,4 @@ from typing import Final
 class Config(ABC):
     CMD_PREFIX: Final = '.'
     ID_GUILD: Final = 1101622403693547630
-    STANDARD_EXTENSIONS: Final = ['owner.loader', 'owner.command']
+    STANDARD_EXTENSIONS: Final = ['owner.loader', 'owner.command', 'owner.presence']
